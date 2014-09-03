50a4588635c48132f1b7570d283da2dc706c783c
anurse <andrew@andrewnurse.net>
2014-09-03 11:50:30 -0700

    Added testing script

---
+++ RunTests.ps1
@@ -0,0 +1,12 @@
+$config = $env:CONFIGURATION
+if(!$config) {
+    $config = "Debug"
+}
+$xunitDir = dir $PsScriptRoot\packages\xunit.runners.* | select -first 1
+$xunit = Join-Path $xunitDir.FullName "tools\xunit.console.exe"
+$asm = Convert-Path "$PsScriptRoot\tests\JsonLD.Test\bin\$config\JsonLD.Test.dll"
+
+$testStartedRegex = [regex]"^##teamcity\[testStarted name='(?<name>[^']*)' flowId='(?<flowId>[^']*)'\]$"
+$testFinishedRegex = [regex]"^##teamcity\[testFinished name='(?<name>[^']*)' duration='(?<duration>[^']*)' flowId='(?<flowId>[^']*)'\]$"
+
+& $xunit $asm -appveyor
